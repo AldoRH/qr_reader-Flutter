a95043019d24b25d94f7cfb5ed9de42c1907bf21
AldoRH <aldo.romeroh@gmail.com>
2024-02-15 12:53:57 -0600

    Create utils
    Maneja el lanzamiento de la petición http o geo

---
+++ lib/utils/utils.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/models/scan_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+
+Future<void> launchUrls(BuildContext context, ScanModel scan) async {
+
+  final Uri _url = Uri.parse(scan.valor);
+
+  print(_url);
+
+  if (scan.tipo == 'http'){
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }else{
+    Navigator.pushNamed(context, 'map', arguments: scan);
+  }
+}
